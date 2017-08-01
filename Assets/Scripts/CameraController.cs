ab9f9119e550807f4da49a44affd0468dd7c6e89
MrElektronix <milovanpelt@gmail.com>
2017-08-01 13:56:56 +0200

    update

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _object;
+
+
+
+    // Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		transform.LookAt(_object);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private int _speed = 4;
+
+    private Rigidbody _rigibody;
+	// Use this for initialization
+	private void Start ()
+	{
+	    _rigibody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+	{
+	    var x = Input.GetAxisRaw("Horizontal") * _speed;
+	    var z = Input.GetAxisRaw("Vertical") * _speed;
+	    _rigibody.velocity = new Vector3(x, 0, z);
+	}
+}
